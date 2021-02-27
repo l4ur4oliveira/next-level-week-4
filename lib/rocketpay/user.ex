1b8a55e3547dd59295949aa82bc70f235483d721
Laura de Oliveira <l4ur4.88@gmail.com>
2021-02-26 22:53:12 -0300

    Aula 3 - Criando usuários com uma conta usando Ecto.Multi

--- lib/rocketpay/user.ex
+++ lib/rocketpay/user.ex
@@ -3,6 +3,7 @@ defmodule Rocketpay.User do
   import Ecto.Changeset
 
   alias Ecto.Changeset
+  alias Rocketpay.Account
 
   @primary_key {:id, :binary_id, autogenerate: true}
 
@@ -15,6 +16,7 @@ defmodule Rocketpay.User do
     field :password, :string, virtual: true
     field :password_hash, :string
     field :nickname, :string
+    has_one :account, Account
 
     timestamps()
   end
@@ -26,8 +28,8 @@ defmodule Rocketpay.User do
     |> validate_length(:password, min: 6)
     |> validate_number(:age, greater_than_or_equal_to: 18)
     |> validate_format(:email, ~r/@/)
-    |>unique_constraint([:email])
-    |>unique_constraint([:nickname])
+    |> unique_constraint([:email])
+    |> unique_constraint([:nickname])
     |> put_password_hash()
   end
 
