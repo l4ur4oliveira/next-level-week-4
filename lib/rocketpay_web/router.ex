da5fdc588b2c06538b3d4263dc69569c1715964e
Laura de Oliveira <l4ur4.88@gmail.com>
2021-02-27 15:26:06 -0300

    Aula 4 - Realizando saques e criação das views

--- lib/rocketpay_web/router.ex
+++ lib/rocketpay_web/router.ex
@@ -13,7 +13,6 @@ defmodule RocketpayWeb.Router do
     post "/users", UsersController, :create
 
     post "/accounts/:id/deposit", AccountsController, :deposit
-
     post "/accounts/:id/withdraw", AccountsController, :withdraw
   end
 
